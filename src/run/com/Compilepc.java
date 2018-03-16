67ea7909e632f6acc2b41e2382c81728ff04b95d
ShivamGupta29 <shivguptam@gmail.com>
2018-03-16 19:41:57 +0530

    first commit

--- src/run/com/Compilepc.java
+++ src/run/com/Compilepc.java
@@ -24,16 +24,11 @@ public class Compilepc extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter(); 
 		try {
-			request.getHeader("runc.jsp");
+			
                String currentDirectory;
 		currentDirectory = System.getProperty("user.dir");
 		System.out.println("Current working directory : "+currentDirectory);
-		      runProcess("gcc Prog1.c");
-		      runProcess("a.exe");
-		     
-		      
-		    
-		      Process pro = Runtime.getRuntime().exec("gcc Prog1.c");
+	Process pro = Runtime.getRuntime().exec("gcc Prog1.c");
 		      InputStream ins=pro.getInputStream();
 		      
 		      String line = null;
@@ -51,25 +46,17 @@ public class Compilepc extends HttpServlet {
 			    while ((line = in.readLine()) != null) {
 			        out.println(  " " + line);
 			    }
-			  
-			    pro = Runtime.getRuntime().exec("a.exe<input.txt");
-			      ins=pro.getInputStream();
-			      
-			      line = null;
-				     in = new BufferedReader(
-				        new InputStreamReader(ins));
-				    while ((line = in.readLine()) != null) {
-				        out.println(  " " + line);
-				    }
-				   
-				    ins=pro.getErrorStream();
-				    line = null;
-				   in = new BufferedReader(
-				        new InputStreamReader(ins));
-				    while ((line = in.readLine()) != null) {
-				        out.println(  " " + line);
-				      
-				    }
+			 out.println("compiled Succesfully");
+			 try
+		        { 
+		         // We are running "dir" and "ping" command on cmd
+		         Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"a.exe<input.txt>output.txt\"");
+		      }
+		        catch (Exception e)
+		        {
+		            System.out.println("HEY Buddy ! U r Doing Something Wrong ");
+		            e.printStackTrace();
+		        }
 				    
 				   
 				 
@@ -79,23 +66,6 @@ public class Compilepc extends HttpServlet {
 		    }
 		
 	}
-		  private static void printLines(String name, InputStream ins) throws Exception {
-			
-		    String line = null;
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(ins));
-		    while ((line = in.readLine()) != null) {
-		        System.out.println(name + " " + line);
-		    }
-		  }
-
-		  private static void runProcess(String command) throws Exception {
-			
-		    Process pro = Runtime.getRuntime().exec(command);
-		    printLines(command + " stdout:", pro.getInputStream());
-		    printLines(command + " stderr:", pro.getErrorStream());
-		    pro.waitFor();
-		    System.out.println(command + " exitValue() " + pro.exitValue());
-		  }
+		
 
 	}
